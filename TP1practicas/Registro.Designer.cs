57b63f083cf220945bee3943fd559333c5d0724b
alanwach0 <prietoalanm@gmail.com>
2024-06-18 00:16:22 -0300

    -Validacion txtnombre y txtapellido - cambio por masked txt para dni y fechanac -clear campos al crear cuenta - Mensajes al ejecutar "crear cuenta nueva"

--- TP1practicas/Registro.Designer.cs
+++ TP1practicas/Registro.Designer.cs
@@ -28,15 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnCrearCuenta = new System.Windows.Forms.Button();
-            this.txtDni = new System.Windows.Forms.TextBox();
-            this.txtFechaNac = new System.Windows.Forms.TextBox();
             this.txtApellido = new System.Windows.Forms.TextBox();
             this.txtNombre = new System.Windows.Forms.TextBox();
             this.lblDni = new System.Windows.Forms.Label();
             this.lblApellido = new System.Windows.Forms.Label();
             this.lblNombre = new System.Windows.Forms.Label();
-            this.lblRegistro = new System.Windows.Forms.Label();
             this.lblFechaNac = new System.Windows.Forms.Label();
             this.txtContraseña2 = new System.Windows.Forms.TextBox();
             this.txtContraseña1 = new System.Windows.Forms.TextBox();
@@ -45,59 +41,40 @@
             this.lblEmail = new System.Windows.Forms.Label();
             this.lblContraseña1 = new System.Windows.Forms.Label();
             this.lblLeyenda = new System.Windows.Forms.Label();
-            this.btnInicioSesion = new System.Windows.Forms.Button();
+            this.mskFechaNac = new System.Windows.Forms.MaskedTextBox();
+            this.mskDni = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCrearCuenta = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // btnCrearCuenta
-            // 
-            this.btnCrearCuenta.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-            this.btnCrearCuenta.Location = new System.Drawing.Point(325, 277);
-            this.btnCrearCuenta.Name = "btnCrearCuenta";
-            this.btnCrearCuenta.Size = new System.Drawing.Size(183, 29);
-            this.btnCrearCuenta.TabIndex = 32;
-            this.btnCrearCuenta.Text = "Crear cuenta";
-            this.btnCrearCuenta.UseVisualStyleBackColor = true;
-            this.btnCrearCuenta.Click += new System.EventHandler(this.btnCrearCuenta_Click);
-            // 
-            // txtDni
-            // 
-            this.txtDni.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-            this.txtDni.Location = new System.Drawing.Point(407, 152);
-            this.txtDni.Name = "txtDni";
-            this.txtDni.Size = new System.Drawing.Size(164, 20);
-            this.txtDni.TabIndex = 31;
-            // 
-            // txtFechaNac
-            // 
-            this.txtFechaNac.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-            this.txtFechaNac.Location = new System.Drawing.Point(407, 126);
-            this.txtFechaNac.Name = "txtFechaNac";
-            this.txtFechaNac.Size = new System.Drawing.Size(164, 20);
-            this.txtFechaNac.TabIndex = 30;
             // 
             // txtApellido
             // 
             this.txtApellido.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-            this.txtApellido.Location = new System.Drawing.Point(407, 100);
+            this.txtApellido.Location = new System.Drawing.Point(367, 126);
+            this.txtApellido.Margin = new System.Windows.Forms.Padding(4);
             this.txtApellido.Name = "txtApellido";
-            this.txtApellido.Size = new System.Drawing.Size(164, 20);
+            this.txtApellido.Size = new System.Drawing.Size(217, 22);
             this.txtApellido.TabIndex = 29;
+            this.txtApellido.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtApellido_KeyPress);
             // 
             // txtNombre
             // 
             this.txtNombre.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-            this.txtNombre.Location = new System.Drawing.Point(407, 73);
+            this.txtNombre.Location = new System.Drawing.Point(367, 93);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(164, 20);
+            this.txtNombre.Size = new System.Drawing.Size(217, 22);
             this.txtNombre.TabIndex = 28;
+            this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
             // 
             // lblDni
             // 
             this.lblDni.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.lblDni.AutoSize = true;
-            this.lblDni.Location = new System.Drawing.Point(215, 160);
+            this.lblDni.Location = new System.Drawing.Point(111, 200);
+            this.lblDni.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDni.Name = "lblDni";
-            this.lblDni.Size = new System.Drawing.Size(32, 13);
+            this.lblDni.Size = new System.Drawing.Size(36, 16);
             this.lblDni.TabIndex = 27;
             this.lblDni.Text = "D.N.I";
             // 
@@ -105,9 +82,10 @@
             // 
             this.lblApellido.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.lblApellido.AutoSize = true;
-            this.lblApellido.Location = new System.Drawing.Point(215, 107);
+            this.lblApellido.Location = new System.Drawing.Point(111, 135);
+            this.lblApellido.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblApellido.Name = "lblApellido";
-            this.lblApellido.Size = new System.Drawing.Size(44, 13);
+            this.lblApellido.Size = new System.Drawing.Size(57, 16);
             this.lblApellido.TabIndex = 26;
             this.lblApellido.Text = "Apellido";
             // 
@@ -115,63 +93,61 @@
             // 
             this.lblNombre.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(215, 80);
+            this.lblNombre.Location = new System.Drawing.Point(111, 101);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
             this.lblNombre.TabIndex = 25;
             this.lblNombre.Text = "Nombre";
-            // 
-            // lblRegistro
-            // 
-            this.lblRegistro.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-            this.lblRegistro.AutoSize = true;
-            this.lblRegistro.Location = new System.Drawing.Point(237, 34);
-            this.lblRegistro.Name = "lblRegistro";
-            this.lblRegistro.Size = new System.Drawing.Size(320, 13);
-            this.lblRegistro.TabIndex = 24;
-            this.lblRegistro.Text = "Para crear una cuenta ingrese los datos solicitados a continuación";
             // 
             // lblFechaNac
             // 
             this.lblFechaNac.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.lblFechaNac.AutoSize = true;
-            this.lblFechaNac.Location = new System.Drawing.Point(215, 133);
+            this.lblFechaNac.Location = new System.Drawing.Point(111, 167);
+            this.lblFechaNac.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblFechaNac.Name = "lblFechaNac";
-            this.lblFechaNac.Size = new System.Drawing.Size(108, 13);
+            this.lblFechaNac.Size = new System.Drawing.Size(135, 16);
             this.lblFechaNac.TabIndex = 23;
             this.lblFechaNac.Text = "Fecha de Nacimiento";
             // 
             // txtContraseña2
             // 
             this.txtContraseña2.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-            this.txtContraseña2.Location = new System.Drawing.Point(407, 230);
+            this.txtContraseña2.Location = new System.Drawing.Point(367, 286);
+            this.txtContraseña2.Margin = new System.Windows.Forms.Padding(4);
             this.txtContraseña2.Name = "txtContraseña2";
-            this.txtContraseña2.Size = new System.Drawing.Size(164, 20);
+            this.txtContraseña2.PasswordChar = '*';
+            this.txtContraseña2.Size = new System.Drawing.Size(217, 22);
             this.txtContraseña2.TabIndex = 39;
             // 
             // txtContraseña1
             // 
             this.txtContraseña1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-            this.txtContraseña1.Location = new System.Drawing.Point(407, 204);
+            this.txtContraseña1.Location = new System.Drawing.Point(367, 254);
+            this.txtContraseña1.Margin = new System.Windows.Forms.Padding(4);
             this.txtContraseña1.Name = "txtContraseña1";
-            this.txtContraseña1.Size = new System.Drawing.Size(164, 20);
+            this.txtContraseña1.PasswordChar = '*';
+            this.txtContraseña1.Size = new System.Drawing.Size(217, 22);
             this.txtContraseña1.TabIndex = 38;
             // 
             // txtEmail
             // 
             this.txtEmail.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-            this.txtEmail.Location = new System.Drawing.Point(407, 178);
+            this.txtEmail.Location = new System.Drawing.Point(367, 222);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(4);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(164, 20);
+            this.txtEmail.Size = new System.Drawing.Size(217, 22);
             this.txtEmail.TabIndex = 37;
             // 
             // lblContraseña2
             // 
             this.lblContraseña2.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.lblContraseña2.AutoSize = true;
-            this.lblContraseña2.Location = new System.Drawing.Point(215, 238);
+            this.lblContraseña2.Location = new System.Drawing.Point(111, 296);
+            this.lblContraseña2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblContraseña2.Name = "lblContraseña2";
-            this.lblContraseña2.Size = new System.Drawing.Size(105, 13);
+            this.lblContraseña2.Size = new System.Drawing.Size(131, 16);
             this.lblContraseña2.TabIndex = 36;
             this.lblContraseña2.Text = "Repita la contraseña";
             // 
@@ -179,9 +155,10 @@
             // 
             this.lblEmail.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Location = new System.Drawing.Point(215, 185);
+            this.lblEmail.Location = new System.Drawing.Point(111, 231);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(87, 13);
+            this.lblEmail.Size = new System.Drawing.Size(109, 16);
             this.lblEmail.TabIndex = 35;
             this.lblEmail.Text = "Ingrese un e-mail";
             // 
@@ -189,38 +166,74 @@
             // 
             this.lblContraseña1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.lblContraseña1.AutoSize = true;
-            this.lblContraseña1.Location = new System.Drawing.Point(215, 211);
+            this.lblContraseña1.Location = new System.Drawing.Point(111, 263);
+            this.lblContraseña1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblContraseña1.Name = "lblContraseña1";
-            this.lblContraseña1.Size = new System.Drawing.Size(112, 13);
+            this.lblContraseña1.Size = new System.Drawing.Size(139, 16);
             this.lblContraseña1.TabIndex = 34;
             this.lblContraseña1.Text = "Ingrese su contraseña";
             // 
             // lblLeyenda
             // 
             this.lblLeyenda.AutoSize = true;
-            this.lblLeyenda.Location = new System.Drawing.Point(417, 371);
+            this.lblLeyenda.Location = new System.Drawing.Point(156, 413);
+            this.lblLeyenda.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLeyenda.Name = "lblLeyenda";
-            this.lblLeyenda.Size = new System.Drawing.Size(0, 13);
+            this.lblLeyenda.Size = new System.Drawing.Size(0, 16);
             this.lblLeyenda.TabIndex = 41;
             this.lblLeyenda.Visible = false;
             // 
-            // btnInicioSesion
+            // mskFechaNac
             // 
-            this.btnInicioSesion.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-            this.btnInicioSesion.Location = new System.Drawing.Point(325, 339);
-            this.btnInicioSesion.Name = "btnInicioSesion";
-            this.btnInicioSesion.Size = new System.Drawing.Size(183, 29);
-            this.btnInicioSesion.TabIndex = 42;
-            this.btnInicioSesion.Text = "Volver al inicio de sesion";
-            this.btnInicioSesion.UseVisualStyleBackColor = true;
-            this.btnInicioSesion.Click += new System.EventHandler(this.btnInicioSesion_Click);
+            this.mskFechaNac.Location = new System.Drawing.Point(367, 155);
+            this.mskFechaNac.Mask = "00/00/0000";
+            this.mskFechaNac.Name = "mskFechaNac";
+            this.mskFechaNac.Size = new System.Drawing.Size(91, 22);
+            this.mskFechaNac.TabIndex = 44;
+            this.mskFechaNac.ValidatingType = typeof(System.DateTime);
+            // 
+            // mskDni
+            // 
+            this.mskDni.Location = new System.Drawing.Point(367, 193);
+            this.mskDni.Mask = "00.000.000";
+            this.mskDni.Name = "mskDni";
+            this.mskDni.Size = new System.Drawing.Size(91, 22);
+            this.mskDni.TabIndex = 45;
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(111, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(480, 18);
+            this.label1.TabIndex = 46;
+            this.label1.Text = "Para crear una cuenta ingrese los datos solicitados a continuación:";
+            // 
+            // btnCrearCuenta
+            // 
+            this.btnCrearCuenta.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnCrearCuenta.Location = new System.Drawing.Point(254, 341);
+            this.btnCrearCuenta.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCrearCuenta.Name = "btnCrearCuenta";
+            this.btnCrearCuenta.Size = new System.Drawing.Size(154, 36);
+            this.btnCrearCuenta.TabIndex = 47;
+            this.btnCrearCuenta.Text = "Crear Cuenta Nueva";
+            this.btnCrearCuenta.UseVisualStyleBackColor = true;
+            this.btnCrearCuenta.Click += new System.EventHandler(this.btnCrearCuenta_Click);
+            this.btnCrearCuenta.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.btnCrearCuenta_KeyPress);
             // 
             // FrmRegistro
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnInicioSesion);
+            this.ClientSize = new System.Drawing.Size(646, 451);
+            this.Controls.Add(this.btnCrearCuenta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mskDni);
+            this.Controls.Add(this.mskFechaNac);
             this.Controls.Add(this.lblLeyenda);
             this.Controls.Add(this.txtContraseña2);
             this.Controls.Add(this.txtContraseña1);
@@ -228,33 +241,27 @@
             this.Controls.Add(this.lblContraseña2);
             this.Controls.Add(this.lblEmail);
             this.Controls.Add(this.lblContraseña1);
-            this.Controls.Add(this.btnCrearCuenta);
-            this.Controls.Add(this.txtDni);
-            this.Controls.Add(this.txtFechaNac);
             this.Controls.Add(this.txtApellido);
             this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.lblDni);
             this.Controls.Add(this.lblApellido);
             this.Controls.Add(this.lblNombre);
-            this.Controls.Add(this.lblRegistro);
             this.Controls.Add(this.lblFechaNac);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmRegistro";
             this.Text = "Registro";
+            this.Load += new System.EventHandler(this.FrmRegistro_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button btnCrearCuenta;
-        private System.Windows.Forms.TextBox txtDni;
-        private System.Windows.Forms.TextBox txtFechaNac;
         private System.Windows.Forms.TextBox txtApellido;
         private System.Windows.Forms.TextBox txtNombre;
         private System.Windows.Forms.Label lblDni;
         private System.Windows.Forms.Label lblApellido;
         private System.Windows.Forms.Label lblNombre;
-        private System.Windows.Forms.Label lblRegistro;
         private System.Windows.Forms.Label lblFechaNac;
         private System.Windows.Forms.TextBox txtContraseña2;
         private System.Windows.Forms.TextBox txtContraseña1;
@@ -263,6 +270,9 @@
         private System.Windows.Forms.Label lblEmail;
         private System.Windows.Forms.Label lblContraseña1;
         private System.Windows.Forms.Label lblLeyenda;
-        private System.Windows.Forms.Button btnInicioSesion;
+        private System.Windows.Forms.MaskedTextBox mskFechaNac;
+        private System.Windows.Forms.MaskedTextBox mskDni;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCrearCuenta;
     }
 }
--- TP1practicas/Registro.cs
+++ TP1practicas/Registro.cs
@@ -26,19 +26,26 @@ namespace TP1practicas
         {
             string nombre = txtNombre.Text;
             string apellido = txtApellido.Text;
-            string fechanacimiento = txtFechaNac.Text;
-            string dni = txtDni.Text;
+            string fechanacimiento = mskFechaNac.Text;
+            string dni = mskDni.Text;
             string email = txtEmail.Text;
             string contraseña1 = txtContraseña1.Text;
             string contraseña2 = txtContraseña2.Text;
 
             if (string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(apellido) || string.IsNullOrEmpty(fechanacimiento) || string.IsNullOrEmpty(dni) || string.IsNullOrEmpty(email) || string.IsNullOrEmpty(contraseña1) || string.IsNullOrEmpty(contraseña2))
             {
-                lblLeyenda.Text = ("debe completar todos los campos, revise si hay algun campo incompleto");
+                lblLeyenda.Text = ("Algun campo es erroneo o se encuentra icompleto");
             }
             else
             {
-                lblLeyenda.Text = ("Carga exitosa");
+                lblLeyenda.Text = ("Usted acaba de crear una Cuenta Nueva");
+                txtNombre.Clear();
+                txtApellido.Clear();
+                mskFechaNac.Clear();
+                mskDni.Clear();
+                txtEmail.Clear();
+                txtContraseña1.Clear();
+                txtContraseña2.Clear();
             }
             lblLeyenda.Visible = true;
             //CARGAMOS LA LISTA
@@ -46,10 +53,57 @@ namespace TP1practicas
 
         private void btnInicioSesion_Click(object sender, EventArgs e)
         {
-            InicioSesion v5 = new InicioSesion();
-            v5.ShowDialog();
+         
+        }
+
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs nptNombre)
+        {
+            if (Char.IsLetter(nptNombre.KeyChar))
+            {
+                nptNombre.Handled = false;
+            }
+            else if (Char.IsSeparator(nptNombre.KeyChar))
+            {
+                nptNombre.Handled = false;
+            }
+            else if (Char.IsControl(nptNombre.KeyChar))
+            {
+                nptNombre.Handled = false;
+            }
+            else
+            {
+                nptNombre.Handled = true;
+            }
+        }
+
+        private void txtApellido_KeyPress(object sender, KeyPressEventArgs nptApellido)
+        {
+            if (Char.IsLetter(nptApellido.KeyChar))
+            {
+                nptApellido.Handled = false;
+            }
+            else if (Char.IsSeparator(nptApellido.KeyChar))
+            {
+                nptApellido.Handled = false;
+            }
+            else if (Char.IsControl(nptApellido.KeyChar))
+            {
+                nptApellido.Handled = false;
+            }
+            else
+            {
+                nptApellido.Handled = true;
+            }
+        }
+
+        private void FrmRegistro_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCrearCuenta_KeyPress(object sender, KeyPressEventArgs e)
+        {
 
         }
     }
-    }
-
+}
